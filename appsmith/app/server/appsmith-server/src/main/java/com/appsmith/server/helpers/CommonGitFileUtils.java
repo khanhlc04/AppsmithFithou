5327b21ac18784d4543ee8f3ec0e49b0f55ea80c
honganhss <114710729+honganhss@users.noreply.github.com>
2024-11-01 18:30:14 +0700

    anhs + src appsmith p2

---
+++ appsmith/app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/CommonGitFileUtils.java
@@ -0,0 +1,37 @@
+package com.appsmith.server.helpers;
+
+import com.appsmith.external.git.FileInterface;
+import com.appsmith.external.git.operations.FileOperations;
+import com.appsmith.external.models.ApplicationGitReference;
+import com.appsmith.git.files.FileUtilsImpl;
+import com.appsmith.server.helpers.ce.CommonGitFileUtilsCE;
+import com.appsmith.server.migrations.JsonSchemaVersions;
+import com.appsmith.server.services.AnalyticsService;
+import com.appsmith.server.services.SessionUserService;
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Import({FileUtilsImpl.class})
+public class CommonGitFileUtils extends CommonGitFileUtilsCE {
+
+    public CommonGitFileUtils(
+            ArtifactGitFileUtils<ApplicationGitReference> applicationGitFileUtils,
+            FileInterface fileUtils,
+            FileOperations fileOperations,
+            AnalyticsService analyticsService,
+            SessionUserService sessionUserService,
+            Gson gson,
+            JsonSchemaVersions jsonSchemaVersions) {
+        super(
+                applicationGitFileUtils,
+                fileUtils,
+                fileOperations,
+                analyticsService,
+                sessionUserService,
+                jsonSchemaVersions);
+    }
+}
